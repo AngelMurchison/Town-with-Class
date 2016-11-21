8ee938018f6be3dd4d065fe0f0f4f55ba4d039be
Angel Murchison <angelmurchison09@gmail.com>
2016-11-21 16:41:43 -0500

    Created each class, gave them a .toString override and q DisplayName method.

---
+++ School/School.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace town
+{
+class School
+    {
+        public string Name { get; set; }
+        public string BuildingType { get; set; }
+        public string DisplayName()
+        {
+            return $"{Name}";
+        }
+        public override string ToString()
+        {
+            return $"{BuildingType}: {Name}";
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ townClass/House.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace town
+{
+    class House
+    {
+        public House()
+        {
+            BuildingType = $"{BuildingType}";
+        }
+        public string Name { get; set; }
+        public string BuildingType { get; set; }
+        public int NumberOfRooms { get; set; }
+        public string ConstructionMaterial { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public int SquareFootage { get; set; }
+        public int Floors { get; set; }
+        public int BedRooms { get; set; }
+        public int BathRooms { get; }
+        public int Volume { get; set; }
+        public string Address1 { get; }
+        public string Address2 { get; }
+        public string city { get; }
+        public string zip { get; }
+        public string FullAddress
+        {
+            get
+            {
+                return $"{Address1}, {Address2}, {city}, {zip}";
+            }
+        }
+        public int CurrentOccupants { get; set; }
+        public int IncreaseOccupants()
+        {
+            return CurrentOccupants += 1;
+        }
+        public int DecreaseOccupants()
+        {
+            return CurrentOccupants -= 1;
+        }
+
+        public string DisplayName()
+        {
+            return $"{Name}";
+        }
+        public override string ToString()
+        {
+            return $"{BuildingType}: {Name}";
+        }
+
+        static void Main(string[] Args)
+        {
+
+        }
+    }
+}
